8c93be7bc7bc520debe5c2ccbe892fdf5f98c191
Doug Sparling <doug@cyberdeck.dev>
2022-04-09 20:07:54 -0400

    Add some docs now that the language has comments.

--- src/main/lisp/aoc2020/lists.lisp
+++ src/main/lisp/aoc2020/lists.lisp
@@ -1,4 +1,5 @@
 (begin
+    (-- map transforms all elements of the given list using the supplied mapping proc)
     (define map (lambda (list mapper)
         (if (eq list nil)
             nil
@@ -6,6 +7,8 @@
                 (mapper (head list))
                 (map (tail list) mapper)))))
 
+    (-- reduce produces a single value by repeatedly applying a combining function on adjacent elements
+        of the list, starting from the left)
     (define reduce (lambda (list reducer)
         (if (eq list nil)
             nil
@@ -13,6 +16,7 @@
                 (head list)
                 (reducer (head list) (reduce (tail list) reducer))))))
 
+    (-- filter returns a list containing only elements matching a predicate)
     (define filter (lambda (list predicate)
         (if (eq list nil)
             nil
@@ -20,12 +24,15 @@
                 (cons (head list) (filter (tail list) predicate))
                 (filter (tail list) predicate)))))
 
+    (-- drop returns a list with the first n elements removed. If n exceeds the length of the
+        list, returns nil)
     (define drop (lambda (list n)
         (if (<= n 0) list (drop (tail list) (- n 1)))))
 
     (define sum (lambda (nums) (reduce nums +)))
     (define product (lambda (nums) (reduce nums *)))
 
+    (-- returns true if some element in the list is equal to the given needle)
     (define contains (lambda (needle haystack)
         (if (eq haystack nil)
             false
@@ -33,6 +40,7 @@
                 true
                 (contains needle (tail haystack))))))
 
+    (-- returns the size of the given list, or 0 if the list is empty)
     (define size (lambda (list)
         (if (eq list nil)
             0
